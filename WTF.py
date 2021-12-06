8db05eb3a138948cb6c7b1e969df23ef36c7eac4
Shamith Mahesh <vidyadg@gmail.com>
2021-12-06 12:37:29 +0530

    removed app.js and made changes to wtf.py

--- WTF.py
+++ WTF.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
 from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField
 from wtforms import validators, ValidationError
-import Account
+from Account.Create_account import Player
 
 class Account(Form):
    name = TextField("Name Of Student",[validators.Required("Please enter your name.")])
